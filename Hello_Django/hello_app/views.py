49734718e2e11bf766f350bf1a5e7c267e67dba4
Ansebi <43926760+Ansebi@users.noreply.github.com>
2022-10-12 02:14:03 +0300

    templates, template inheritance done

--- Hello_Django/hello_app/views.py
+++ Hello_Django/hello_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 11, 2022'
+    },
+    {
+        'author': 'Author 2',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 12, 2022'
+    }
+]
 
 
 def home(request):
-    return render(request, 'hello_app/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'hello_app/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About...</h1>')
+    return render(request, 'hello_app/about.html', {'title': 'An About Page'})
