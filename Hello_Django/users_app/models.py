084f3a1d2aa23bca9e724394c3a207e488e3097e
Ansebi <43926760+Ansebi@users.noreply.github.com>
2022-10-19 02:16:02 +0300

    user can edit their profile

--- Hello_Django/users_app/models.py
+++ Hello_Django/users_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
+THUMBNAIL_MAX_DIMENSION = 500
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,4 +11,18 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        super().save()
+        img = Image.open(self.image.path)
+        if any([
+            img.height > THUMBNAIL_MAX_DIMENSION,
+            img.width > THUMBNAIL_MAX_DIMENSION
+        ]):
+            img.thumbnail((THUMBNAIL_MAX_DIMENSION, THUMBNAIL_MAX_DIMENSION))
+            img.save(self.image.path)
+
+
 
