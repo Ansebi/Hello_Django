2a4c35babaccc6e8cf83c1db9a57290f6f48b464
Ansebi <43926760+Ansebi@users.noreply.github.com>
2022-11-02 21:38:30 +0300

    ...

--- Hello_Django/Hello_Django/settings.py
+++ Hello_Django/Hello_Django/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +26,7 @@ SECRET_KEY = 'django-insecure-#j=kn3r)adw%*q_perehg7bg)(tyan(jwlh$j_b(5*8(e^1l7-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HELLO_DJANGO_IP')]
-
+ALLOWED_HOSTS = [os.environ['HELLO_DJANGO_IP']]
 
 # Application definition
 
